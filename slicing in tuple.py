8c8d2f3cdcb8ca4659d0e848007cf852f5115495
AKHLAK ANSARI <87934721+AkhlakAnsari@users.noreply.github.com>
2021-09-12 00:11:17 +0530

    Add files via upload

---
+++ slicing in tuple.py
@@ -0,0 +1,16 @@
+# Accessing tuple elements using slicing
+my_tuple = ('p','r','o','g','r','a','m','i','z')
+
+# elements 2nd to 4th
+print(my_tuple[1:4])
+
+# elements beginning to 2nd
+print(my_tuple[:-7])
+
+# elements 8th to end
+print(my_tuple[7:])
+
+# elements beginning to end
+# Output: ('p', 'r', 'o', 'g', 'r', 'a', 'm', 'i', 'z')
+print(my_tuple[:])
+#thanks akhlakansari94@gmail.com
